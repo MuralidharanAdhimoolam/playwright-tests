4f89a7919346d7e4470cbec402413ac224d6f7f0
MuralidharanAdhimoolam <adhi549@gmail.com>
2026-05-17 12:18:57 +0200

    Update demo.spec.js

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('Assertion test', async ({ page }) => {
   await page.goto ('https://kitchen.applitools.com/')
-  //await expect(page.locator('text=The Kitchen')).toHaveCount(1);
+  await expect(page.locator('text=The Kitchen')).toHaveCount(1);
   await expect(page.getByRole('heading', { name: 'The Kitchen' })).toBeVisible({ timeout: 15000 });
   await page.pause()
-  await page.$('heading', { name: 'The Kitchen' })
+  //await page.$('heading', { name: 'The Kitchen' })
 });
